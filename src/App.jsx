2c5101432ae0cca7e231728bb53726543948cc68
nyteksf <email.nytek@gmail.com>
2025-04-15 18:15:32 -0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-cyan-100 to-cyan-300">
+      <h1 className="text-4xl font-bold text-gray-900">Landio Clone Works!</h1>
+    </div>
+  );
+}
