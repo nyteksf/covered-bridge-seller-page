27d57ab865851d60eb11839662e39b17ff855491
nyteksf <email.nytek@gmail.com>
2025-07-10 20:12:53 -0700

    Update anchor to React Link to prevent Vercel error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import mapGraphic from "../../public/ripple-bg-optimized.svg";
 
 export default function Hero() {
@@ -13,20 +15,20 @@ export default function Hero() {
       {/* Content */}
       <div className="absolute z-1 text-center max-w-2xl left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col md:flex-row gap-6 justify-center">
-          <a
-            href="/land-for-sale?country=U.S.A."
+          <Link
+            to="/land-for-sale?country=U.S.A."
             className="hover:text-cyan-200 hover:border-white border-4 border-cyan-200 px-6 pt-1.5 pb-[0.35rem] font-montserrat text-[22px] text-center mt-0 mb-0 uppercase cursor-pointer bg-[#131313] rounded-none flex-none transition-all duration-200 font-extrabold h-[52px] w-[251.1px] py-3 shadow-[0_0_4px_rgba(0,255,255,0.35)] hover:shadow-[0_0_6px_rgba(255,255,255,0.5)] -tracking-[0.0325px]"
           >
             Buy Property
-          </a>
-          <a
-            href="https://www.selltocoveredbridge.com/"
+          </Link>
+          <Link
+            to="https://www.selltocoveredbridge.com/"
             rel="noopener noreferrer"
             target="_blank"
             className="hover:text-cyan-200 hover:border-white border-4 border-cyan-200 px-6 pt-1.5 pb-[0.35rem] font-montserrat text-[22px] text-center mt-0 mb-0 uppercase cursor-pointer bg-[#131313] rounded-none flex-none transition-all duration-200 font-extrabold h-[52px] w-[251.1px] py-3 shadow-[0_0_4px_rgba(0,255,255,0.35)] hover:shadow-[0_0_6px_rgba(255,255,255,0.5)] -tracking-[0.0325px]"
           >
             Sell Property
-          </a>
+          </Link>
         </div>
       </div>
     </section>
