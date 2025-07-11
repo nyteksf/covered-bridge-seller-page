db55d56c491d13296e3ef6f2fd49e69315585525
nyteksf <email.nytek@gmail.com>
2025-07-10 21:26:47 -0700

    Change available states, StateSelector.jsx

--- src/components/StateSelector.jsx
+++ src/components/StateSelector.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function StateSelector() {
-  const availableStates = ["Texas"];
+  const availableStates = ["Texas", "Florida", "Oklahoma"];
 
   const stateAbbreviations = {
     Alabama: "AL",
