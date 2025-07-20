c95e31c6f10f21e5219935e5b82b61d05aa0351e
nyteksf <email.nytek@gmail.com>
2025-07-20 01:24:15 -0700

    Restrict state search to florida, StateSelector.jsx

--- src/components/StateSelector.jsx
+++ src/components/StateSelector.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function StateSelector() {
-  const availableStates = ["Texas", "Florida", "Oklahoma"];
+  const availableStates = ["Florida"];
 
   const stateAbbreviations = {
     Alabama: "AL",
